941a7401c74b860344187c236e29a1f9558d444f
iacantek <alan.cantekin@hslu.ch>
2023-12-29 16:10:26 +0100

    feat: height and weight conversion, BMI colored accordingly in history

--- backend.php
+++ backend.php
@@ -21,10 +21,26 @@ function query_json_result($conn, $queryString, $single = false)
                 'weight' => $row['weight'],
                 'bmi' => $row['bmi']
             ];
+
+            // only fetch first record if single request
+            if ($single) {
+                break;
+            }
         }
     }
 
     return json_encode($single ? $calculations[0] : $calculations);
+}
+
+function throw_error($status_code, $message)
+{
+    http_response_code($status_code);
+    die(json_encode(['error' => $message]));
+}
+
+function bad_request($message)
+{
+    throw_error(400, $message);
 }
 
 // database connection
@@ -37,23 +53,52 @@ if (!$conn) {
 $method = $_SERVER['REQUEST_METHOD'];
 $body = file_get_contents("php://input");
 
-if ($method == 'GET') {
+if ($method == 'GET') { // GET request -> returns all calculations
     echo(query_json_result($conn, "SELECT * FROM calculation ORDER BY id DESC"));
-} else if ($method == 'POST') {
+} else if ($method == 'POST') { // POST request -> creates calculation record and returns it
     // check if body is empty
     if (!$body) {
-        http_response_code(400);
-        die(json_encode(['error' => "Body cannot be empty!"]));
+        bad_request("Body cannot be empty.");
     }
 
     $form = json_decode($body);
+    $birthday = null;
 
-    $bmi = (double)$form->weight->value / ((((double)$form->height->value) / 100) ^ 2);
+    // check if birthday is an invalid date
+    try {
+        $birthday = new DateTime($form->birthday);
+    } catch (Exception $e) {
+        bad_request("Birthday is not a valid date.");
+    }
+
+    // check if are fields are set
+    if (!isset($form->name) || !isset($form->birthday) || !isset($form->gender) ||
+        !isset($form->height->value) || !isset($form->height->unit) ||
+        !isset($form->weight->value) || !isset($form->weight->unit)) {
+        bad_request("Please fill in all fields.");
+    } // check if birthday is in future
+    else if (new DateTime() < $birthday) {
+        bad_request("Birthday cannot lie in the future.");
+    } // check if height is a negative number
+    else if ($form->height->value < 0) {
+        bad_request("Height has to be a positive number.");
+    } // check if weight is a negative number
+    else if ($form->weight->value < 0) {
+        bad_request("Weight has to be a positive number.");
+    }
+
+    // convert weight to kg if it's in lbs
+    $weight = $form->weight->unit == 0 ? $form->weight->value : $form->weight->value / 2.2;
+    // convert height to cm if it's in ft
+    $height = $form->height->unit == 0 ? $form->height->value : $form->height->value * 30.48;
+
+    // calculate bmi
+    $bmi = $weight / ((($height) / 100) ^ 2);
 
     $statement = "insert into calculation (name, birthday, gender, height, weight, bmi) values (?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $statement);
 
-    mysqli_stmt_bind_param($stmt, 'ssssss', $form->name, $form->birthday, $form->gender, $form->height->value, $form->weight->value, $bmi);
+    mysqli_stmt_bind_param($stmt, 'ssssss', $form->name, $form->birthday, $form->gender, $height, $weight, $bmi);
     $res = mysqli_stmt_execute($stmt);
 
     // return creted record as json
--- js/vue.js
+++ js/vue.js
@@ -25,10 +25,6 @@ Vue.createApp({
                     height: 300, // max height in cm
                     weight: 999 // max weight in kg
                 },
-                operand: {
-                    weight: 2.2, // kg to lbs
-                    height: 30.48 // cm to foot
-                },
                 unit: {
                     height: {
                         cm: 0,
@@ -43,6 +39,7 @@ Vue.createApp({
                     male: 0,
                     female: 1
                 },
+                genders: ["männlich", "weiblich"],
                 today: new Date().toISOString().split('T')[0]
             }
         }
@@ -66,10 +63,14 @@ Vue.createApp({
                 .then(response => {
                     this.history.unshift(response);
                     this.bmi = response.bmi;
+                    // TODO: draw canvas
                 })
                 .catch(error => {
                     console.error(error);
                 });
+        },
+        drawCanvas() {
+
         },
         validateFields() {
             let fieldNames = ["name", "birthday", "height", "weight"];
@@ -81,34 +82,34 @@ Vue.createApp({
                 case "name":
                     if (!this.form.name) { // check if name is empty
                         this.validations['name'] = RequiredValidationMessage.replace("%s", "Vollständiger Name");
-                    } else {
+                    } else { // clear validation
                         delete this.validations['name'];
                     }
                     break;
                 case "birthday":
                     if (!this.form.birthday) { // check if birthday is empty
                         this.validations['birthday'] = RequiredValidationMessage.replace("%s", "Geburtstag");
-                    } else if (this.form.birthday > new Date()) { // check that birthday is not in the future
+                    } else if (this.form.birthday > new Date()) { // check if birthday is not in the future
                         this.validations['birthday'] = "Geburtstag darf nicht in der Zukunft liegen."
-                    } else {
+                    } else { // clear validation
                         delete this.validations['birthday'];
                     }
                     break;
                 case "height":
                     if (!this.form.height.value) { // check if height is empty
                         this.validations['height'] = RequiredValidationMessage.replace("%s", "Grösse");
-                    } else if (this.form.height.value < 0) {
+                    } else if (this.form.height.value < 0) { // check if height is a negative number
                         this.validations['height'] = NegativeNumberValidationMessage
-                    } else {
+                    } else { // clear validation
                         delete this.validations['height'];
                     }
                     break;
                 case "weight":
                     if (!this.form.weight.value) { // check if weight is empty
                         this.validations['weight'] = RequiredValidationMessage.replace("%s", "Gewicht");
-                    } else if (this.form.weight.value < 0) {
+                    } else if (this.form.weight.value < 0) { // check if weight is a negative number
                         this.validations['weight'] = NegativeNumberValidationMessage;
-                    } else {
+                    } else { // clear validation
                         delete this.validations['weight'];
                     }
                     break;
@@ -145,6 +146,23 @@ Vue.createApp({
                     xhr.send();
                 }
             });
+        },
+        getBMIColor(bmi) {
+            colorBMI = null;
+            if (bmi < 18.5) {
+                colorBMI = "#1e9ad6"; // blue
+            } else if (bmi < 25) {
+                colorBMI = "#30a248"; // green
+            } else if (bmi < 30) {
+                colorBMI = "#f6ca35"; // yellow
+            } else if (bmi < 40) {
+                colorBMI = "#f78a1f"; // orange
+            } else {
+                colorBMI = "#b92a30"; // red
+            }
+            return {
+                color: colorBMI
+            };
         }
     }
 }).mount('#app');
